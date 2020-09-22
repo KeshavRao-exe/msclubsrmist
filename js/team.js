7347edc2f55d53cd1ebdd874575b0014238688b8
Akshat Khanna <khannakshat7@gmail.com>
2020-09-22 23:56:39 +0530

    Update events and team changes

--- js/team.js
+++ js/team.js
@@ -3,12 +3,10 @@
         document.getElementById("coreimg").style.display="block"; 
         document.getElementById("techimg").style.display="none";
         document.getElementById("creativesimg").style.display="none";
-        document.getElementById("primg").style.display="none";
         document.getElementById("sponsimg").style.display="none";
         document.getElementById("corebtn").style.backgroundColor="#02A4EF";
         document.getElementById("techbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("creativesbtn").style.backgroundColor="#bfbfbf";
-        document.getElementById("prbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("sponsbtn").style.backgroundColor="#bfbfbf";
     }
     
@@ -16,13 +14,11 @@
         document.getElementById("coreimg").style.display="none"; 
         document.getElementById("techimg").style.display="block";
         document.getElementById("creativesimg").style.display="none";
-        document.getElementById("primg").style.display="none";
         document.getElementById("sponsimg").style.display="none";
         
         document.getElementById("corebtn").style.backgroundColor="#bfbfbf";
         document.getElementById("techbtn").style.backgroundColor="#02A4EF";
         document.getElementById("creativesbtn").style.backgroundColor="#bfbfbf";
-        document.getElementById("prbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("sponsbtn").style.backgroundColor="#bfbfbf";
     }
     
@@ -30,13 +26,11 @@
         document.getElementById("coreimg").style.display="none"; 
         document.getElementById("techimg").style.display="none";
         document.getElementById("creativesimg").style.display="block";
-        document.getElementById("primg").style.display="none";
         document.getElementById("sponsimg").style.display="none"; 
 
         document.getElementById("corebtn").style.backgroundColor="#bfbfbf";
         document.getElementById("techbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("creativesbtn").style.backgroundColor="#02A4EF";
-        document.getElementById("prbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("sponsbtn").style.backgroundColor="#bfbfbf";
     }
     
@@ -59,13 +53,11 @@
         document.getElementById("coreimg").style.display="none"; 
         document.getElementById("techimg").style.display="none";
         document.getElementById("creativesimg").style.display="none";
-        document.getElementById("primg").style.display="none";
         document.getElementById("sponsimg").style.display="block";
 
         document.getElementById("corebtn").style.backgroundColor="#bfbfbf";
         document.getElementById("techbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("creativesbtn").style.backgroundColor="#bfbfbf";
-        document.getElementById("prbtn").style.backgroundColor="#bfbfbf";
         document.getElementById("sponsbtn").style.backgroundColor="#02A4EF";
     }
     
